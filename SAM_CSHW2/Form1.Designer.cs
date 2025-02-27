afa784df23d4387eac6d60fb226bdc5c6a131d8b
srstepanek <sstepanek@ltu.edu>
2025-02-27 16:50:35 -0500

    Updated HW2 to complete HW3
    Updated HW2 to complete HW3

---
+++ SAM_CSHW2/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace SAM_CSHW2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.GrayPictureBox = new System.Windows.Forms.PictureBox();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GrayPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.BackColor = System.Drawing.SystemColors.Control;
+            this.PictureBox.Location = new System.Drawing.Point(30, 32);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(290, 248);
+            this.PictureBox.TabIndex = 0;
+            this.PictureBox.TabStop = false;
+            // 
+            // GrayPictureBox
+            // 
+            this.GrayPictureBox.Location = new System.Drawing.Point(464, 166);
+            this.GrayPictureBox.Name = "GrayPictureBox";
+            this.GrayPictureBox.Size = new System.Drawing.Size(588, 456);
+            this.GrayPictureBox.TabIndex = 1;
+            this.GrayPictureBox.TabStop = false;
+            this.GrayPictureBox.Click += new System.EventHandler(this.GrayPictureBox_Click);
+            // 
+            // trackBar
+            // 
+            this.trackBar.Location = new System.Drawing.Point(464, 722);
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(588, 114);
+            this.trackBar.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(464, 646);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 38);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(585, 646);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 38);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(706, 646);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 38);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(829, 646);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 38);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(952, 646);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 38);
+            this.textBox5.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1415, 932);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.GrayPictureBox);
+            this.Controls.Add(this.PictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FromClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GrayPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox emguPictureBox;
+        private System.Windows.Forms.PictureBox BinaryThreshHoldBox;
+        private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.PictureBox GrayPictureBox;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ SAM_CSHW2/Form1.cs
@@ -0,0 +1,136 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SAM_CSHW2
+{
+
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture;
+        private Thread _captureThread;
+       
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _capture = new VideoCapture(0);
+            _captureThread = new Thread(DisplayWebcam);
+            _captureThread.Start(); 
+
+        }
+        private void DisplayWebcam()
+        {
+            while(_capture.IsOpened)
+            {
+                //Raw Image code
+                Mat frame = _capture.QueryFrame();
+                
+
+                int newHeight = (frame.Size.Height * PictureBox.Size.Width) / frame.Size.Width;
+                Size newSize = new Size(PictureBox.Size.Width, newHeight);
+               
+                CvInvoke.Resize(frame, frame, newSize);
+
+
+                PictureBox.Image = frame.ToBitmap();
+
+
+                //Code for a grayscale and binary-threshholding for GrayPictureBox
+
+                int newgrayHeight = (frame.Size.Height * GrayPictureBox.Size.Width) / frame.Size.Width;
+                Size newgraySize = new Size(GrayPictureBox.Size.Width, newgrayHeight);
+
+                CvInvoke.Resize(frame, frame, newgraySize);
+
+
+                Mat grayFrame = new Mat();
+                CvInvoke.CvtColor(frame, grayFrame, ColorConversion.Bgr2Gray);
+
+                
+                Mat binaryFrame = new Mat();
+                //CvInvoke.Threshold(grayFrame, binaryFrame, 120, 255, ThresholdType.Binary | ThresholdType.Otsu);
+                CvInvoke.AdaptiveThreshold(grayFrame, binaryFrame,255, AdaptiveThresholdType.MeanC, ThresholdType.BinaryInv, 11, 2);
+
+
+                //Split GrayFrame into 5 slices...
+
+                int width = binaryFrame.Width;
+                int height = binaryFrame.Height;
+                int Slicewidth = width / 5;
+                int[] whitePixelCounts = new int[5];
+
+
+                for (int i = 0; i < 5; i++) 
+                {
+                    Rectangle roi = new Rectangle(i * Slicewidth, 0, Slicewidth, height);
+                    Mat slice = new Mat(binaryFrame, roi);
+                    whitePixelCounts[i] = CvInvoke.CountNonZero(slice);
+                    //Update lable with white pixle count 
+
+                    this.Invoke((MethodInvoker)delegate {
+                        textBox1.Text = $"1st: {whitePixelCounts[0]}";
+                        textBox2.Text = $"2nd: {whitePixelCounts[1]}";
+                        textBox3.Text = $"3rd: {whitePixelCounts[2]}";
+                        textBox4.Text = $"4th: {whitePixelCounts[3]}";
+                        textBox5.Text = $"5th: {whitePixelCounts[4]}";
+                    });
+
+
+
+                    CvInvoke.Rectangle(binaryFrame, roi, new MCvScalar(0, 255, 0), 2);
+                
+                }
+
+
+                GrayPictureBox.Image = binaryFrame.ToBitmap();
+
+                
+
+
+
+            }
+
+
+
+        }
+        private void Form1_FromClosing(object sender, FormClosingEventArgs e)
+        {
+            _captureThread.Abort();
+        }
+
+
+
+
+
+       
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GrayPictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
